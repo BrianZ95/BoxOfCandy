3b889ef26b1f0245301945d04d9808be355c1c36
BrianZ95 <brianz0905@gmail.com>
2025-02-26 14:26:56 +0000

    Second Commit

---
+++ Candy.java
@@ -0,0 +1,16 @@
+public class Candy {
+    private String flavor;
+    /** Returns a String representing the flavor of this piece of candy */
+    public Candy(String f){
+        flavor=f;
+    }
+    public String toString()
+    {
+        return flavor;
+    }
+    public String getFlavor() {
+        /* implementation not shown */ }
+
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
